f414330ca10b0639935be0be07036b83dfeb99a8
hanahh080601 <hanlehngoc080601@gmail.com>
2022-09-11 10:22:17 +0700

    first commit on reactjs project

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+
+function ExpenseItem(props) {
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+      <button onClick={() => {console.log("Clicked!")}}>Change title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
