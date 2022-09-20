36e9c42e10c8b4dd109c5203d20674c238790131
hanlhn <hanlhn@bap.jp>
2022-09-20 10:34:15 +0700

    Practice with Fragments, Portals & Refs in React

---
+++ Fragments_Portals_Refs/src/components/Users/AddUser.js
@@ -0,0 +1,59 @@
+import React, { useState, useRef } from "react";
+import classes from "./AddUser.module.css";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
+
+const AddUser = (props) => {
+  const [error, setError] = useState("");
+  const enteredName = nameInputRef.current.value;
+  const enteredUserAge = ageInputRef.current.value;
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
+      return;
+    }
+    if (+enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (greater than 0).",
+      });
+      return;
+    }
+    props.onAddUser(enteredUsername, enteredAge);
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
+  };
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input id="username" type="text" ref={nameInputRef}></input>
+          <label htmlFor="age">Age (Years)</label>
+          <input id="age" type="number" ref={ageInputRef}></input>
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
+  );
+};
+
+export default AddUser;
